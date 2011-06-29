38e3807989bdd37e4e38d13d815840bf7c607404
Julien Thevenon <julien_thevenon@yahoo.fr>
2011-06-29 02:14:51 +0200

    Creating a widget dedicated to base facture content : date, reference, livre_facture_id, status

---
+++ include/base_facture_widget.h
@@ -0,0 +1,65 @@
+#ifndef _BASE_FACTURE_WIDGET_H_
+#define _BASE_FACTURE_WIDGET_H_
+
+#include "facture_status.h"
+
+#include <inttypes.h>
+#include <QWidget>
+#include <vector>
+
+class QWidget;
+class my_date_widget;
+class QComboBox;
+class fichier_client;
+
+class base_facture_widget: public QWidget
+{
+  Q_OBJECT
+    public:
+  base_facture_widget(QWidget * p_parent,
+		      fichier_client & p_fichier_client
+		      );
+
+  void set_enabled(bool p_enabled);
+  void set_allowed_facture_ref(const std::vector<uint32_t> & p_list);
+  void set_allowed_livre_ids(const std::vector<uint32_t> & p_livre_ids);
+  void set_status_list(const std::vector<facture_status> & p_status_list);
+
+  bool is_date_complete(void)const;
+  bool is_date_empty(void)const;
+  const std::string get_iso_date(void)const;
+  uint32_t get_facture_reference(void)const;
+  uint32_t get_livre_facture_id(void)const;
+  const facture_status * get_facture_status(void)const;
+
+  void clear_date(void);
+
+  private slots:
+  void date_entered(void);
+  void facture_ref_selected(int);
+  void livre_facture_selected(int);
+  void status_selected(int);
+
+ signals:
+  void new_date_entered(void);
+  void new_facture_ref_selected(void);
+  void new_livre_facture_selected(void);
+  void new_status_selected(void);
+ private:
+  uint32_t get_livre_facture_id(int p_index)const;
+  uint32_t get_facture_reference(int p_index)const;
+  const facture_status * get_facture_status(int p_index)const;
+
+  void set_combo_box_content(QComboBox * p_combo_box,std::vector<uint32_t> & p_current, const std::vector<uint32_t> & p_list);
+  void clear_combo_box_content(QComboBox * p_combo_box);
+
+  fichier_client & m_fichier_client;
+  my_date_widget * m_date_field;
+  QComboBox * m_livre_facture_id_field;
+  QComboBox * m_facture_reference_field;
+  QComboBox * m_status_field;
+  std::vector<uint32_t> m_livre_facture_ids;
+  std::vector<uint32_t> m_facture_references;
+  std::vector<facture_status> m_status;
+};
+#endif
---
+++ src/base_facture_widget.cpp
@@ -0,0 +1,265 @@
+#include "base_facture_widget.h"
+
+#include "fichier_client.h"
+#include "my_date_widget.h"
+#include "facture.h"
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QComboBox>
+#include <sstream>
+#include <iostream>
+
+//------------------------------------------------------------------------------
+base_facture_widget::base_facture_widget(QWidget * p_parent,
+					   fichier_client & p_fichier_client
+					   ):
+  QWidget(p_parent),
+  m_fichier_client(p_fichier_client),
+  m_date_field(NULL),
+  m_livre_facture_id_field(NULL),
+  m_facture_reference_field(NULL),
+  m_status_field(NULL)
+{
+  QHBoxLayout *l_horizontal_layout = new QHBoxLayout(this);
+
+  // Date
+  l_horizontal_layout->addWidget(new QLabel(tr("Date")+" :"));
+  m_date_field = new my_date_widget();
+  l_horizontal_layout->addWidget(m_date_field);
+  connect(m_date_field,SIGNAL(date_ok()),this,SLOT(date_entered()));
+   
+  // Livre facture
+  l_horizontal_layout->addWidget(new QLabel(tr("Livre Facture")+" :"));
+  m_livre_facture_id_field = new QComboBox(this);
+  m_livre_facture_id_field->setSizeAdjustPolicy(QComboBox::AdjustToContents);
+  l_horizontal_layout->addWidget(m_livre_facture_id_field);
+  //  connect(m_livre_facture_id_field,SIGNAL(currentIndexChanged (int)),this,SLOT(livre_facture_selected(int)));
+  connect(m_livre_facture_id_field,SIGNAL(activated (int)),this,SLOT(livre_facture_selected(int)));
+   
+  // Facture reference
+  l_horizontal_layout->addWidget(new QLabel(tr("Facture reference")+" :"));
+  m_facture_reference_field = new QComboBox(this);
+  m_facture_reference_field->setSizeAdjustPolicy(QComboBox::AdjustToContents);
+  l_horizontal_layout->addWidget(m_facture_reference_field);
+  connect(m_facture_reference_field,SIGNAL(activated (int)),this,SLOT(facture_ref_selected(int))); 
+
+  // Facture status
+  l_horizontal_layout->addWidget(new QLabel(tr("Status")+" :"));
+  m_status_field = new QComboBox(this);
+  m_status_field->setSizeAdjustPolicy(QComboBox::AdjustToContents);
+  connect(m_status_field, SIGNAL(activated (int)),this,SLOT(status_selected(int)));
+  l_horizontal_layout->addWidget(m_status_field);
+}
+
+//------------------------------------------------------------------------------
+void base_facture_widget::set_enabled(bool p_enabled)
+{
+  m_date_field->setEnabled(p_enabled);;
+  m_livre_facture_id_field->setEnabled(p_enabled);
+  m_facture_reference_field->setEnabled(p_enabled);
+  m_status_field->setEnabled(p_enabled);
+}
+
+//------------------------------------------------------------------------------
+void base_facture_widget::date_entered(void)
+{
+  std::cout << "QtEvent::base_facture_widget::date_entered : \"" << m_date_field->get_iso_date() << "\"" << std::endl ;
+  emit new_date_entered();
+}
+
+//------------------------------------------------------------------------------
+void base_facture_widget::facture_ref_selected(int p_index)
+{
+  std::cout << "QtEvent::base_facture_widget::Facture_reference_selected index " << p_index << std::endl;
+  uint32_t l_ref = get_facture_reference();
+  if(l_ref)
+    {
+      std::cout << "QtEvent::base_facture_widget::Facture_reference_selected " << l_ref << std::endl;
+    }
+  emit new_facture_ref_selected();
+}
+
+//------------------------------------------------------------------------------
+void base_facture_widget::livre_facture_selected(int p_index)
+{
+  std::cout << "QtEvent::base_facture_widget::livre_facture_selected index " << p_index << std::endl;
+  uint32_t l_ref = get_livre_facture_id();
+  if(l_ref)
+    {
+      std::cout << "QtEvent::base_facture_widget::Livre_facture selected " << l_ref << std::endl;
+    }
+  emit new_livre_facture_selected();
+}
+
+//------------------------------------------------------------------------------
+void base_facture_widget::status_selected(int p_index)
+{
+  std::cout << "QtEvent::base_facture_widget::status_selected index " << p_index << std::endl;
+  const facture_status * l_ptr_facture_status = get_facture_status();
+  if(l_ptr_facture_status)
+    {
+      std::cout << "QtEvent::base_facture_widget::status selected " << *l_ptr_facture_status << std::endl;
+    }
+  emit new_status_selected();
+}
+
+//------------------------------------------------------------------------------
+void base_facture_widget::clear_date(void)
+{
+  m_date_field->setText("");
+}
+
+//------------------------------------------------------------------------------
+bool base_facture_widget::is_date_complete(void)const
+{
+  return m_date_field->is_complete();
+}
+
+//------------------------------------------------------------------------------
+bool base_facture_widget::is_date_empty(void)const
+{
+  return m_date_field->is_empty();
+}
+
+//------------------------------------------------------------------------------
+const std::string base_facture_widget::get_iso_date(void)const
+{
+  assert(m_date_field->is_complete());
+  return m_date_field->get_iso_date();
+}
+
+//------------------------------------------------------------------------------
+uint32_t base_facture_widget::get_livre_facture_id(int p_index)const
+{
+  uint32_t l_result = 0;
+  // Check if something is selected
+  if(p_index>0 || (!p_index && m_livre_facture_ids.size() == 1))
+    {
+      uint32_t l_real_index = (m_livre_facture_ids.size() > 1 ? p_index - 1 : 0);
+      assert(l_real_index < m_livre_facture_ids.size());
+      l_result = m_livre_facture_ids[l_real_index];
+    }
+  return l_result;
+}
+
+//------------------------------------------------------------------------------
+uint32_t base_facture_widget::get_facture_reference(int p_index)const
+{
+  uint32_t l_result = 0;
+  // Check if something is selected
+  if(p_index>0 || (!p_index && m_facture_references.size() == 1))
+    {
+      uint32_t l_real_index = (m_facture_references.size() > 1 ? p_index - 1 : 0);
+      assert(l_real_index < m_facture_references.size());
+      l_result = m_facture_references[l_real_index];
+    }
+  return l_result;
+}
+
+//------------------------------------------------------------------------------
+const facture_status * base_facture_widget::get_facture_status(int p_index)const
+{
+  const facture_status * l_result = NULL;
+    // Check if something is selected
+  if(p_index>0 || (!p_index && m_status.size() == 1))
+    {
+      uint32_t l_real_index = (m_status.size() > 1 ? p_index - 1 : 0);
+      assert(l_real_index < m_status.size());
+      l_result = &(m_status[l_real_index]);
+    }
+  return l_result;
+}
+
+
+//------------------------------------------------------------------------------
+uint32_t base_facture_widget::get_livre_facture_id(void)const
+{
+  return get_livre_facture_id(m_livre_facture_id_field->currentIndex());
+}
+
+//------------------------------------------------------------------------------
+uint32_t base_facture_widget::get_facture_reference(void)const
+{
+  return get_facture_reference(m_facture_reference_field->currentIndex());
+}
+
+
+//------------------------------------------------------------------------------
+const facture_status * base_facture_widget::get_facture_status(void)const
+{
+  return get_facture_status(m_status_field->currentIndex());
+}
+
+//------------------------------------------------------------------------------
+void base_facture_widget::clear_combo_box_content(QComboBox * p_combo_box)
+{
+  assert(p_combo_box);
+  while(p_combo_box->count())
+    {
+      p_combo_box->removeItem(0);
+    }
+}
+
+//------------------------------------------------------------------------------
+void base_facture_widget::set_combo_box_content(QComboBox * p_combo_box,std::vector<uint32_t> & p_current,const std::vector<uint32_t> & p_list)
+{
+  clear_combo_box_content(p_combo_box);
+  p_current.clear();
+  if(p_list.size() > 1)
+    {
+      p_combo_box->addItem("");
+    }
+
+  std::vector<uint32_t>::const_iterator l_iter = p_list.begin();
+  std::vector<uint32_t>::const_iterator l_iter_end = p_list.end();
+  while(l_iter != l_iter_end)
+    {
+      QString l_ref;
+      l_ref.setNum(*l_iter);
+      p_combo_box->addItem(l_ref);
+      p_current.push_back(*l_iter);
+      ++l_iter;
+    }
+  if(p_combo_box->count())
+    {
+      p_combo_box->setCurrentIndex(0);
+    }
+}
+
+//------------------------------------------------------------------------------
+void base_facture_widget::set_allowed_facture_ref(const std::vector<uint32_t> & p_list)
+{
+  set_combo_box_content(m_facture_reference_field,m_facture_references,p_list);
+}
+
+//------------------------------------------------------------------------------
+void base_facture_widget::set_allowed_livre_ids(const std::vector<uint32_t> & p_livre_ids)
+{
+  set_combo_box_content(m_livre_facture_id_field,m_livre_facture_ids,p_livre_ids);
+}
+
+//------------------------------------------------------------------------------
+void base_facture_widget::set_status_list(const std::vector<facture_status> & p_status_list)
+{
+  clear_combo_box_content(m_status_field);
+  m_status.clear();
+  if(p_status_list.size() > 1)
+    {
+      m_status_field->addItem("");
+    }
+  std::vector<facture_status>::const_iterator l_iter = p_status_list.begin();
+  std::vector<facture_status>::const_iterator l_iter_end = p_status_list.end();
+  while(l_iter != l_iter_end)
+    {
+      m_status_field->addItem(l_iter->getName().c_str());
+      m_status.push_back(*l_iter);
+      ++l_iter;
+    }
+  if(m_status_field->count())
+    {
+      m_status_field->setCurrentIndex(0);
+    }
+}
+
+//EOF
